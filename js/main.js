653feb96d520bf1aa7bf345ed75a694106f8eb20
Lira Artem <artemlira84@gmail.com>
2022-06-02 01:26:06 +0300

    описал логику вычисления валют, начал добавлять реверс, пока не доделал

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@ class CurrencyConverter {
       this.wrapper = document.querySelector(wrapper);
       this.optins = this.wrapper.querySelector(optins);
       this.main = this.wrapper.querySelector(main);
-      this.object = {};
+      this.globalCurrency = {};
+      this.firstCurrency;
+      this.secondCurrency;
    }
 
    addTodayDate() {
@@ -29,37 +31,84 @@ class CurrencyConverter {
       const request = new XMLHttpRequest();
       request.addEventListener('readystatechange', () => {
          if (request.readyState === 4 && request.status === 200) {
-            this.object = JSON.parse(request.responseText);
-            // console.dir(this.object);
-            this.calc();
+            let json = JSON.parse(request.responseText);
+            json.forEach((v) => {
+               this.globalCurrency[v.ccy] = v;
+            })
          }
       });
       request.open('GET', 'https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11');
-      // request.setRequestHeader('Content-type', 'application/json');
       request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
       request.send();
    }
 
    calc() {
-      this.main.addEventListener('input', (even) => {
+      let sum = 0;
+      let firstValue = this.main.querySelector('.currency__1').querySelector('input').value;
+      this.main.querySelector('.copy_1').querySelector('input').value = firstValue;
+      console.dir(this.main.querySelector('.copy_2').querySelector('option').value);
+      if (this.firstCurrency == 'UAH') {
+         sum = firstValue / this.globalCurrency[this.secondCurrency].sale;
+      } else if (this.secondCurrency == 'UAH') {
+         sum = firstValue * this.globalCurrency[this.firstCurrency].buy;
+      } else {
+         sum = (firstValue * this.globalCurrency[this.firstCurrency].buy) / this.globalCurrency[this.secondCurrency].sale;
+      }
+      this.main.querySelector('.original').querySelector('input').value = sum.toFixed(2);
+      this.main.querySelector('.copy_2').querySelector('input').value = sum.toFixed(2);
+   }
+
+   eventsFirstButton() {
+      this.firstCurrency = this.main.querySelector('.currency__1').querySelector('option').value;
+      this.firstCurrency = this.main.querySelector('.copy_2').querySelector('option').value;
+      this.main.querySelector('.currency__1').querySelector('select').addEventListener('change', (even) => {
          let target = even.target;
-         if (target.closest('.currency__1')) {
-            let firstValue = this.main.querySelector('.currency__1').querySelector('input').value;
-            console.dir(firstValue);
-         }
-      });
-      this.main.querySelector('select').addEventListener('change', (even) => {
-         let t = even.target.value;
-         console.dir(t);
+         this.firstCurrency = target.value;
       });
    }
 
+   eventsSecondButton() {
+      this.secondCurrency = this.main.querySelector('.currency__2').querySelector('option').value;
+      this.secondCurrency = this.main.querySelector('.copy_1').querySelector('option').value;
+      this.main.querySelector('.currency__2').querySelector('select').addEventListener('change', (even) => {
+         let target = even.target;
+         this.secondCurrency = target.value;
+      });
+   }
+
+   showResult() {
+      this.main.addEventListener('click', (even) => {
+         let target = even.target;
+         if (target.closest('.btn_1')) {
+            this.calc();
+         }
+      })
+   }
+
+   reverse() {
+      this.main.addEventListener('click', (even) => {
+         let target = even.target;
+         if (target.closest('.btn_2')) {
+            console.dir(this.main.querySelector('.copy_1').querySelector('option').value);
+            this.main.querySelector('.copy_2').classList.toggle('reverse');
+            this.main.querySelector('.copy_1').classList.toggle('reverse');
+            this.main.querySelector('.currency__1').classList.toggle('reverse2');
+            this.main.querySelector('.currency__2').classList.toggle('reverse2');
+         }
+      });
+   }
+
+
+
+
    init() {
-      console.dir(this);
+      console.dir(this.globalCurrency);
       this.addTodayDate();
-      // this.choicePayment();
       this.gettingData();
-      // this.calc();
+      this.eventsFirstButton();
+      this.eventsSecondButton();
+      this.reverse();
+      this.showResult();
    }
 }
 
